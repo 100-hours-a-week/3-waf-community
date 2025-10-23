c75fe7ddfb347080677bd7dfef6704ed700be256
WAFriend3416 <127187396+WAFriend3416@users.noreply.github.com>
2025-10-23 17:22:03 +0900

    chore: 기존 src_fe/ 디렉토리 제거 (모노레포 전환 준비)

--- src_fe/scripts/pages/board/write.js
+++
@@ -1,5 +0,0 @@
-// Board Write Script - TODO: 구현 필요
-(function(window, document) {
-  'use strict';
-  // Config, State, Elements, Init, Events, API, Render...
-})(window, document);
--- src_fe/scripts/pages/user/password-change.js
+++
@@ -1,5 +0,0 @@
-// Password Change Script - TODO: 구현 필요
-(function(window, document) {
-  'use strict';
-  // Config, State, Elements, Init, Events, API, Validation...
-})(window, document);
